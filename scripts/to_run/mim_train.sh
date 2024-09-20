f9e028ef8ace80285b51255857f71bc75e1c2214
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2024-09-20 15:55:47 -0700

    Update mim_train.sh

--- scripts/to_run/mim_train.sh
+++ scripts/to_run/mim_train.sh
@@ -9,6 +9,5 @@ cp /project/rrg-kyi/astro/hsc/HSC_dud_galaxy_GIRYZ7610_64_new.h5 $SLURM_TMPDIR
 cp /project/rrg-kyi/astro/hsc/HSC_dud_simple_classifier_data_GIRYZ7610_64.h5 $SLURM_TMPDIR
 cp /project/rrg-kyi/astro/hsc/HSC_dud_simple_regressor_data_GIRYZ7610_64.h5 $SLURM_TMPDIR
 
-# then for purple plotting:
 python pretrain_mim.py mim_test5_multigpu_paper -v 1000 -ct 10.00 -dd $SLURM_TMPDIR/
 
